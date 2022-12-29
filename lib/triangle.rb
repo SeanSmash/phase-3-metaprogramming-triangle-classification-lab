fa3eaa9cc83efab4da1b45b25068d8f7e90766ad
Sean Sarokas <sps4462@gmail.com>
2022-12-29 07:26:23 -0500

    commit tree 2bdbcc01774a9e88b4d956afa9d8aff6f64cc879

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,41 @@
+require 'pry'
+
 class Triangle
   # write code here
+  attr_accessor :one, :two, :three
+
+  def initialize(one, two, three)
+    @one = one
+    @two = two
+    @three = three
+    @sides = []
+    @sides.push(one, two, three)
+    #binding.pry
+    @sides.filter do |i|
+      if i <= 0
+        raise TriangleError
+      end
+    end
+    if one + two <= three
+      raise TriangleError
+    elsif one + three <= two
+      raise TriangleError
+    elsif two + three <= one
+      raise TriangleError
+    end
+  end
+
+  def kind
+    #binding.pry
+    if @sides.uniq.length == 1
+      return :equilateral
+    elsif @sides.uniq.length == 2
+      return :isosceles
+    elsif @sides.uniq.length == 3
+      return :scalene
+    end
+  end
+  
+  class TriangleError < StandardError
+  end
 end
